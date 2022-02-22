c7f419daa0d873ae4a94d742680696e59ece604c
3R1KD4N <88765036+3R1KD4N@users.noreply.github.com>
2022-02-22 00:53:49 -0600

    Add files via upload

--- scritps/menuLogic.js
+++ scritps/menuLogic.js
@@ -1,36 +1,48 @@
 const gridArea = document.querySelector(".gridArea");
+const gridSelection = document.querySelector(".gridSelection");
 
-/*
-	menuBtn.addEventListener("click", ()=>{
-	if(acordionMenu.className == "acordionMenu open"){
-		setTimeout(()=>{
-			acordionMenu.className = "acordionMenu close";
-		}, 350);
-		acordionItems.forEach((e)=>{
-			e.className = "acordionItem noVisible";
-		});
-	} else{
-		acordionMenu.className = "acordionMenu open";
-		setTimeout(()=>{
-			acordionItems.forEach((e)=>{
-				e.className = "acordionItem visible";
-			});
-		}, 350);
-	}
-});
-*/
-
+const debugText = document.querySelector(".debugText");
 const navDom = document.querySelector(".navDom");
 
-window.onload = setMenuType();
+const buttonSender = document.getElementById("buttonSender").addEventListener("click", (e)=>{
+	e.preventDefault();
+	sendEmail();
+});
 
-window.addEventListener('resize', (e)=>{
-	setMenuType();
-})
+function sendEmail(){
+	const name = document.getElementById("floatingName");
+	const email = document.getElementById("floatingEmail");
+	const theme = document.getElementById("floatingTheme");
+	const body = document.getElementById("floatingBody");
 
-function setMenuType(){
-	if(window.screen.width > 900){
-		console.log(window.screen.width);
+	var flag = 0;
+	if((/^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$/).test(email.value) && name.value != "" && theme.value != "" && body.value != ""){
+		const validationAlert = document.getElementById("validationAlert").className = "alert alert-success visually";
+		flag = 1;
+	}else{
+		const validationAlert = document.getElementById("validationAlert").classList.replace("visually-hidden", "visually");
+		flag = 0;
+	}
+
+	/*Query Sender*/
+	if(flag == 1){
+		const mailto = document.querySelector(".mailto");
+
+		mailto.setAttribute('href', `mailto:medellinjimenezerikdaniel2@gmail.com?subject=`+name.value+`&body=De: `+email.value+`, `+body.value);
+		mailto.click();
+
+		email.value = "";
+		name.value = "";
+		theme.value = "";
+		body.value = "";
+	}
+}
+
+function setMenuType(screenWidth){
+
+	var screenMenuWidth = screenWidth;
+
+	if(screenMenuWidth > 1090){
 		navDom.innerHTML = `<ul class="navbarUl">
 			<li><a href=""><img src="./assets/mainLogo.png" alt="Smith Mantenimiento" class="mainLogoNav"></a></li>
 			<div>
@@ -101,38 +113,7 @@ function setMenuType(){
 	}
 }
 
-/*
-	<nav class="shadow-lg">
-            <ul class="navbarUl">
-                <li><a href=""><img src="./assets/mainLogo.png" alt="Smith Mantenimiento" class="mainLogoNav"></a></li>
-                <li><img src="./assets/menuButton.png" alt="menu" class="menuButton" id="menuBtn"></li>
-            </ul>
-            <div class="acordionMenu close">
-                <ul class="acordionUl">
-                    <li class="acordionItem noVisible">
-                        <a href="./nosotros.html">
-                            <h3 class="h3Text">Sobre Nosotros</h3>
-                            <div class="divisionBar"></div>
-                        </a>
-                    </li>
-                    <li class="acordionItem noVisible">
-                        <a href="./servicios.html">
-                            <h3 class="h3Text">Servicios</h3>
-                            <div class="divisionBar"></div>
-                        </a>
-                    </li>
-                    <li class="acordionItem noVisible">
-                        <a href="">
-                            <h3 class="h3Text">Rational MTY</h3>
-                            <div class="divisionBar"></div>
-                        </a>
-                    </li>
-                    <li class="acordionItem noVisible">
-                        <a href="">
-                            <h3 class="h3Text">Contacto</h3>
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-*/
+
+window.addEventListener('resize', ()=>{
+	setMenuType(navDom.clientWidth);
+})
